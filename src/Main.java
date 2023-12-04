8b8bdbe9f55bc0138a02ed9f15398826b7f7d6df
Safiq Momin <39603717+safiqmomin@users.noreply.github.com>
2023-12-03 23:08:30 -0700

    Update Main.java
    Added comments

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to the game!");
         char[][] board = new char[3][3];
         for (int row = 0; row < board.length; row++) {
             for (int col = 0; col <board[row].length; col++) {
@@ -47,6 +47,7 @@ public class Main {
 
     }
 
+    //Returns true if the player has won
     private static boolean haveWon(char[][] board, char player) {
         for (int row = 0; row < board.length; row++) {
             if (board[row][0] == player && board[row][1] == player && board[row][2] == player) {
@@ -64,6 +65,7 @@ public class Main {
         return (board[0][0] == player && board[1][1] == player && board[2][2] == player) || (board[0][2] == player && board[1][1] == player && board[2][0] == player);
     }
 
+    //Prints the board
     private static void printBoard(char[][] board) {
         for (char[] chars : board) {
             for (int col = 0; col < chars.length; col++) {
